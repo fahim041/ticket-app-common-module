51320ca32aa798e79bc133efde3b5cd8c1c54405
M0nster92 <fahimalsafi@gmail.com>
2023-07-22 10:48:19 -0400

    additional config

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let color = {
+    red: 0,
+    green: 0,
+    blue: 0,
+};
+console.log(color);
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+let color: Color = {
+  red: 0,
+  green: 0,
+  blue: 0,
+};
+
+console.log(color);
+
+export default color;
